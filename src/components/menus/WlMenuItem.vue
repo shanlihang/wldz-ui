98970e1942c2c17a0982d3487ee7cb2517e7ff29
shanlihang <slh_ontuo@126.com>
2024-02-06 13:40:43 +0800

    菜单项的设计

---
+++ src/components/menus/WlMenuItem.vue
@@ -0,0 +1,35 @@
+<!-- 菜单项组件 -->
+<template>
+  <div class="container">
+    <img :src="props.src" style="width:50px;height:50px;">
+    <span>{{props.value}}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {defineProps} from 'vue'
+const props = defineProps({
+    value:{
+        type:String,
+        default:'菜单项'
+    },
+    src:{
+      type:String,
+      default:'./cancel.png'
+    }
+})
+</script>
+
+<style scoped>
+.container{
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+.container:hover{
+    background-color: aqua;
+    color: #fff;
+}
+</style>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import WlButton from '../components/base/WlButton.vue'
+import WlMenuItem from '../components/menus/WlMenuItem.vue'
 </script>
 
 <template>
-    <div><WlButton></WlButton></div>
+    <div><WlMenuItem value="员工管理" src="../components/menus/a.png"></WlMenuItem></div>
 </template>
 
 <style scoped>
 div{
-    width: 100%;
+    width: 200px;
     height: 100vh;
     display: flex;
     justify-content: center;
