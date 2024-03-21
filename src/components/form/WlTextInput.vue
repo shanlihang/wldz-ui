47e5f2957c290eb6d463328a4da774446fe1c59e
shanlihang <slh_ontuo@126.com>
2024-03-21 16:04:28 +0800

    去除id属性

--- src/components/form/WlTextInput.vue
+++ src/components/form/WlTextInput.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 
 interface WlInput{
-    id?:string,
     placeholder?:string,
     width?:string,
     height?:string,
@@ -9,7 +8,6 @@ interface WlInput{
 }
 
 const props = withDefaults(defineProps<WlInput>(),{
-    id:'input-a',
     width:'100%',
     height:'30px',
     placeholder:'请输入内容',
@@ -20,19 +18,16 @@ const props = withDefaults(defineProps<WlInput>(),{
 
 <template>
     <div class="wl-input">
-        <div class="input">
-            <input
-                type="text" 
-                :id="props.id" 
-                :placeholder="props.placeholder"
-                :disabled="props.disabled"
-                :style="{
-                    width:props.width,
-                    height:props.height,
-                    
-                }"
-            >
-        </div>
+        <input
+            type="text" 
+            :placeholder="props.placeholder"
+            :disabled="props.disabled"
+            :style="{
+                width:props.width,
+                height:props.height,
+                
+            }"
+        >
     </div>
 </template>
 
@@ -44,20 +39,14 @@ const props = withDefaults(defineProps<WlInput>(),{
     justify-content: center;
 }
 
-.input{
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-.input input{
+.wl-input input{
     border-radius: 6px;
     border: 1px solid #E0E0E0;
 	box-shadow:none;
 	outline: none;
     text-indent: 1rem;
 }
-.input input:focus{
+.wl-input input:focus{
     border: 1px solid #00a0fd;
 }
 </style>
