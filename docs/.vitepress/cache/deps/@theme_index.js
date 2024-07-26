bf80f7920140f8de592f8e845864d6c6f79e5408
slh_ytkj <67490009@qq.com>
2024-07-26 08:20:06 +0800

    项目规整

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -9,33 +9,33 @@ import {
 } from "./chunk-HQVAGNKX.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default4 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default5 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
-import { default as default6 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default7 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default8 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default9 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default10 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default11 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default12 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default13 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default14 } from "F:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default4 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default5 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
+import { default as default6 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default7 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default8 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default9 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default10 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default11 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default12 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default13 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default14 } from "D:/wldz/wldz-ui/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/vitepress/dist/client/theme-default/support/utils.js
 import { withBase } from "vitepress";
